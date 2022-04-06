6b557bd7268962fccc3964de6c7ff10c63e5d54a
Chris Strohfeldt <chris@animaginedreality.com>
2022-04-06 10:24:29 +1000

    (#11) Added extra test.

--- Packages/Fluxity/Tests/DispatcherHandleTests.cs
+++ Packages/Fluxity/Tests/DispatcherHandleTests.cs
@@ -1,4 +1,5 @@
 using AIR.Fluxity;
+using AIR.Fluxity.Tests.DummyTypes;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -17,5 +18,22 @@ public class DispatcherHandleTests
 
         // Assert
         dispatcher.Received().Dispatch(Arg.Any<ICommand>());
+    }
+
+    [Test]
+    public void Dispatch_WhenCalledWithCommand_ShouldHaveDispatcherDispatchSameCommand()
+    {
+        // Arrange
+        const int PAYLOAD_VALUE = 1;
+        var dispatcher = Substitute.For<IDispatcher>();
+        var dispatcherHandle = new DispatcherHandle();
+        var command = new DummyCommand { payload = PAYLOAD_VALUE };
+        dispatcherHandle.Inject(dispatcher);
+
+        // Act
+        dispatcherHandle.Dispatch(command);
+
+        // Assert
+        dispatcher.Received().Dispatch(Arg.Is<DummyCommand>(x => x == command));
     }
 }
