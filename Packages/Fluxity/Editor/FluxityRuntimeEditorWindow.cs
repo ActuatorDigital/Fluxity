7ce0bb24e6e53d5820cfbfac59b50f3811e0ac21
Steve Halliwell <steve@animaginedreality.com>
2023-02-27 13:26:44 +1000

    (#31) Editor window binding changes.

---
+++ Packages/Fluxity/Editor/FluxityRuntimeEditorWindow.cs
@@ -0,0 +1,71 @@
+﻿using AIR.Flume;
+using UnityEditor;
+
+namespace AIR.Fluxity.Editor
+{
+    internal class FluxityRuntimeEditorWindow : EditorWindow
+    {
+        internal class FluxityHandle : Dependent
+        {
+            public IStore Store { get; private set; }
+            public IDispatcher Dispatcher { get; private set; }
+
+            public void Inject(
+                IStore store,
+                IDispatcher dispatcher)
+            {
+                Store = store;
+                Dispatcher = dispatcher;
+            }
+        }
+
+        protected FluxityHandle _fluxityHandle;
+
+        public virtual void OnEnable()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        public virtual void OnDisable()
+        {
+            EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+        }
+
+        protected virtual void Refresh()
+        {
+            _fluxityHandle = null;
+        }
+
+        protected virtual IStore GetStore()
+        {
+            if (_fluxityHandle == null)
+            {
+                if (EditorApplication.isPlaying)
+                {
+                    _fluxityHandle = new FluxityHandle();
+                }
+            }
+
+            return _fluxityHandle?.Store;
+        }
+
+        protected virtual IDispatcher GetDispatcher()
+        {
+            if (_fluxityHandle == null)
+            {
+                if (EditorApplication.isPlaying)
+                {
+                    _fluxityHandle = new FluxityHandle();
+                }
+            }
+
+            return _fluxityHandle?.Dispatcher;
+        }
+
+        protected virtual void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            Refresh();
+            Repaint();
+        }
+    }
+}
--- Packages/Fluxity/Editor/StoreWindow.cs
+++ Packages/Fluxity/Editor/StoreWindow.cs
@@ -1,24 +1,14 @@
 using UnityEngine;
 using UnityEditor;
-using AIR.Flume;
 using System.Linq;
 
 namespace AIR.Fluxity.Editor
 {
-    class StoreWindow : EditorWindow
+    internal class StoreWindow : FluxityRuntimeEditorWindow
     {
-        internal class StoreHandle : Dependent
-        {
-            public IStore Store { get; private set; }
-
-            public void Inject(IStore store)
-                => Store = store;
-        }
-
         private const int LeftPanelRightWall = 220;
         private Vector2 _leftPanelScrollViewPos;
         private Vector2 _rightPanelScrollViewPos;
-        private StoreHandle _storeHandle;
         private string _currentlyShowingName;
         private FeatureStateDrawer _featureDrawer;
 
@@ -29,16 +19,6 @@ namespace AIR.Fluxity.Editor
             var window = GetWindow<StoreWindow>();
             window.titleContent = new GUIContent("Fluxity Runtime Stores");
             window.Show();
-        }
-
-        public void OnEnable()
-        {
-            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
-        }
-
-        public void OnDisable()
-        {
-            EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
         }
 
         public void OnGUI()
@@ -52,6 +32,13 @@ namespace AIR.Fluxity.Editor
                 DoStoreSelectPanel();
                 DoSelectedStore();
             }
+        }
+
+        protected override void Refresh()
+        {
+            base.Refresh();
+            _currentlyShowingName = null;
+            _featureDrawer = null;
         }
 
         private void DoStoreSelectPanel()
@@ -76,7 +63,7 @@ namespace AIR.Fluxity.Editor
                     else
                     {
                         EditorGUILayout.LabelField("Stores", EditorStyles.boldLabel);
-                        DrawHorLine(Color.grey);
+                        EditorWindowUtil.DrawHorLine(Color.grey);
                         foreach (var feat in store.GetAllFeatures())
                         {
                             var name = feat.GetType().GenericTypeArguments[0].Name;
@@ -109,7 +96,7 @@ namespace AIR.Fluxity.Editor
                     else
                     {
                         EditorGUILayout.LabelField($"Values of {_currentlyShowingName}", EditorStyles.boldLabel);
-                        DrawHorLine(Color.grey);
+                        EditorWindowUtil.DrawHorLine(Color.grey);
 
                         drawer.Draw();
                     }
@@ -132,42 +119,6 @@ namespace AIR.Fluxity.Editor
 
             _featureDrawer = new FeatureStateDrawer(targetFeat, _currentlyShowingName);
             return _featureDrawer;
-        }
-
-        private void Refresh()
-        {
-            _storeHandle = null;
-            _currentlyShowingName = null;
-            _featureDrawer = null;
-        }
-
-        private IStore GetStore()
-        {
-            if (_storeHandle == null)
-            {
-                if (EditorApplication.isPlaying)
-                {
-                    _storeHandle = new StoreHandle();
-                }
-            }
-
-            return _storeHandle?.Store;
-        }
-
-        private static void DrawHorLine(Color color, int thickness = 2, int padding = 10)
-        {
-            Rect r = EditorGUILayout.GetControlRect(GUILayout.Height(padding + thickness));
-            r.height = thickness;
-            r.y += padding / 2;
-            r.x -= 2;
-            r.width += 6;
-            EditorGUI.DrawRect(r, color);
-        }
-
-        private void OnPlayModeStateChanged(PlayModeStateChange obj)
-        {
-            Refresh();
-            Repaint();
         }
     }
 }
