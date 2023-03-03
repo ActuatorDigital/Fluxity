cd52a07dfd86f423c81b7e9496c831e7b5e01d99
Steve Halliwell <steve@animaginedreality.com>
2023-03-03 14:56:53 +1000

    (#31) History window added.

---
+++ Packages/Fluxity/Editor/HistoryWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace AIR.Fluxity.Editor
+{
+    class HistoryWindow : FluxityRuntimeEditorWindow
+    {
+        [MenuItem("Window/Fluxity/Runtime History")]
+        public static void ShowWindow()
+        {
+            // Get existing open window or if none, make a new one:
+            var window = GetWindow<HistoryWindow>();
+            window.titleContent = new GUIContent("Fluxity Runtime History");
+            window.Show();
+        }
+
+        public void OnGUI()
+        {
+            if (!EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                GUILayout.Label("Not in play mode.");
+            }
+            else
+            {
+            }
+        }
+    }
+}
