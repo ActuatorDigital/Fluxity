26ddb7b87dd57a3611776b4f4eb8a5f4a59749bf
Steve Halliwell <steve@animaginedreality.com>
2023-12-13 15:43:33 +1000

    (#40) Clean. Organise feature and reducer setup classes

---
+++ Packages/Fluxity/Runtime/FluxityFeatureContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AIR.Fluxity
+{
+    public class FluxityFeatureContext<TState> : FluxityFlumeRegisterContext
+        where TState : struct
+    {
+        private readonly Feature<TState> _feature;
+        public FluxityFeatureContext(FluxityFlumeRegisterContext context, Feature<TState> feature)
+            : base(context.Store, context.Distpatcher, context.FlumeServiceContainer)
+        {
+            _feature = feature;
+        }
+
+        public FluxityFeatureContext<TState> BulkReducers(Action<FluxityFeatureContext<TState>> registerAll)
+        {
+            registerAll(this);
+            return this;
+        }
+
+        public FluxityFeatureContext<TState> Reducer<TCommand>(IReducer<TState, TCommand>.ReduceDelegate pureFunctionReducer)
+            where TCommand : ICommand
+        {
+            var reducer = new PureFunctionReducerBinder<TState, TCommand>(pureFunctionReducer);
+            _feature.Register(reducer);
+            return this;
+        }
+    }
+}
---
+++ Packages/Fluxity/Runtime/FluxityFlumeRegisterContext.cs
@@ -0,0 +1,26 @@
+﻿using AIR.Flume;
+
+namespace AIR.Fluxity
+{
+    public class FluxityFlumeRegisterContext
+    {
+        public FluxityFlumeRegisterContext(Store store, Dispatcher distpatcher, FlumeServiceContainer flumeServiceContainer)
+        {
+            Store = store;
+            Distpatcher = distpatcher;
+            FlumeServiceContainer = flumeServiceContainer;
+        }
+
+        public Store Store { get; }
+        public Dispatcher Distpatcher { get; }
+        public FlumeServiceContainer FlumeServiceContainer { get; }
+
+        public FluxityFeatureContext<TState> Feature<TState>(TState startingValue)
+            where TState : struct
+        {
+            var feat = new Feature<TState>(startingValue);
+            Store.AddFeature(feat);
+            return new FluxityFeatureContext<TState>(this, feat);
+        }
+    }
+}
